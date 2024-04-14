ec7a9f8916640fde758c5085d33d19bc29f525cf
mgdad3331 <73651002+mgdad3331@users.noreply.github.com>
2024-04-14 20:55:35 +0200

    Update config.js

--- config.js
+++ config.js
@@ -28,7 +28,7 @@ module.exports = {
           logging: false,
         }),
   HANDLERS: (process.env.PREFIX || '^[.,!]').trim(),
-  SUDO: process.env.SUDO || '14_4_63a2_4df1_d4f1',
+  SUDO: process.env.SUDO || '14_4_bc09_669d_8254',
   HEROKU_APP_NAME: process.env.HEROKU_APP_NAME,
   HEROKU_API_KEY: process.env.HEROKU_API_KEY,
   BRANCH: 'master',
